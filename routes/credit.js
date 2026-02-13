b4ece90900db644aa8f4bfaa7f25ac67bd2ed978
DILZAIB <dilzaib5848@gmail.com>
2026-02-13 06:26:45 +0500

    update

--- routes/credit.js
+++ routes/credit.js
@@ -10,7 +10,7 @@ const { CreatePayment, UpdateEscrow, getTransactionsById } = require("../control
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
